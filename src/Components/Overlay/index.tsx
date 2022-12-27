f82a95448d1edc38d490cab282242a6e25f9f3a2
Girish Sawant <girishsawant999.gs@gmail.com>
2022-12-27 17:16:34 +0530

    perf: :zap: improved performance of overlay component

--- src/Components/Overlay/index.tsx
+++ src/Components/Overlay/index.tsx
@@ -1,42 +1,58 @@
-import React, { useEffect } from 'react';
-import { clsx } from '../../utils';
-import { CrossButton } from '../Button';
-import './style.scss';
+import React, { useEffect, useState } from "react";
+import { clsx } from "../../utils";
+import { CrossButton } from "../Button";
+import "./style.scss";
 
 export interface IOverlay {
-  children: React.ReactNode;
-  open: boolean;
-  onClose: () => void;
-  theme?: 'light' | 'dark';
-  coords?: {
-    x: number;
-    y: number;
-  };
+	children: React.ReactNode;
+	open: boolean;
+	onClose: () => void;
+	theme?: "light" | "dark";
+	coords?: {
+		x: number;
+		y: number;
+	};
 }
-const Overlay = ({ children, open, onClose, theme = 'light', coords }: IOverlay): JSX.Element => {
-  useEffect(() => {
-    if (open) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.removeProperty('overflow');
-    }
-  }, [open]);
+const Overlay = ({ children, open, onClose, theme = "light", coords }: IOverlay): JSX.Element => {
+	const [innerHeight, setInnerHeight] = useState(window.innerHeight);
 
-  return (
-    <div
-      data-show={open}
-      data-theme={theme}
-      className={clsx('overlayContainer')}
-      style={
-        {
-          '--transform-coords': coords ? `${coords.x}px ${coords.y}px` : 'center',
-        } as React.CSSProperties
-      }
-    >
-      <CrossButton ripple={false} onClick={onClose} className="close-button" />
-      <div className={clsx('overlayContent')}>{children}</div>
-    </div>
-  );
+	useEffect(() => {
+		if (open) {
+			document.body.style.overflow = "hidden";
+		} else {
+			document.body.style.removeProperty("overflow");
+		}
+	}, [open]);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setInnerHeight(window.innerHeight);
+		};
+		window.addEventListener("resize", handleResize);
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
+	return (
+		<div
+			data-show={open}
+			data-theme={theme}
+			className={clsx("overlayContainer")}
+			style={
+				{
+					"--transform-coords": coords ? `${coords.x}px ${coords.y}px` : "center",
+					"--window-height": innerHeight
+				} as React.CSSProperties
+			}>
+			<CrossButton ripple={false} onClick={onClose} className="close-button" />
+			<div className={clsx("overlayContent")}>
+				{window.innerHeight}
+				{children}
+				{window.innerHeight}
+			</div>
+		</div>
+	);
 };
 
 export default Overlay;
